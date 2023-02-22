e22613a9df673f1e574c99a85df29567580b3f0a
HeleneFuchs <114170310+HeleneFuchs@users.noreply.github.com>
2023-02-22 18:20:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-from urllib.error import URLError
+#from urllib.error import URLError
 
 streamlit.title("My Parents Mew Healty Diner")
 streamlit.header('Breakfast Menu')
@@ -24,7 +24,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 # try: 
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
 # if not fruit_choice:
 #  streamlit.error("Please select a fruit to get information.")
 # else:
